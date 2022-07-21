bb36c720b131d5529ee6a7d73727060a139b2cd6
Ernesto Rodriguez <erodriguez@nerdssupport.com>
2022-07-20 21:04:06 -0400

    added wipe by counter. added best fight percentage

--- obtain_wipes_and_deaths.py
+++ obtain_wipes_and_deaths.py
@@ -48,15 +48,23 @@ sheet = workbook.worksheets[0]
 COL_LOGS = 'B'
 COL_DATE = 'C'
 COL_WIPES = 'A'
-COL_CES = 'F'
-COL_JAMIE = 'G'
-COL_FAYTH = 'H'
-COL_EVE = 'I'
-COL_KAEDE = 'J'
-COL_MITHRIL = 'K'
-COL_FERESIA = 'L'
-COL_RALF = 'M'
+COL_CES = 'G'
+COL_JAMIE = 'H'
+COL_FAYTH = 'I'
+COL_EVE = 'J'
+COL_KAEDE = 'K'
+COL_MITHRIL = 'L'
+COL_FERESIA = 'M'
+COL_RALF = 'N'
 COL_AVERAGE_FIGHT_PERCENTAGE = 'D'
+COL_BEST_FIGHT_PERCENTAGE = 'E'
+COL_PHASE1 = 'O'
+COL_PHASE2 = 'P'
+COL_PHASE3 = 'Q'
+COL_PHASE4 = 'R'
+COL_PHASE_INT = 'S'
+COL_PHASE5 = 'T'
+COL_PHASE6 = 'U'
 
 access_token = obtain_access_token()
 
@@ -77,6 +85,7 @@ for i in range(3,sheet.max_row):
                         id
                         endTime
                         fightPercentage
+                        lastPhase
                     }}
                 }}
             }}
@@ -106,11 +115,41 @@ for i in range(3,sheet.max_row):
         sheet[f"{COL_RALF}{i}"] = int(0)
 
         # Iterate through each fight in the report and add up the deaths.
+        best_percentage = 100.0
         fight_counter = 0
         sum = 0.00
+        p1_wipe_counter = 0
+        p2_wipe_counter = 0
+        p3_wipe_counter = 0
+        p4_wipe_counter = 0
+        int_wipe_counter = 0
+        p5_wipe_counter = 0
+        p6_wipe_counter = 0
+
         for fight in fights['data']['reportData']['report']['fights']:
             fight_counter += 1
             sum += fight['fightPercentage']
+
+            if (float(fight['fightPercentage']) < best_percentage):
+                best_percentage = float(fight['fightPercentage'])
+
+            match int(fight['lastPhase']):
+                case 1:
+                    if (float(fight['fightPercentage']) > 80.0):
+                        p1_wipe_counter += 1
+                    elif (float(fight['fightPercentage']) < 80.0):
+                        int_wipe_counter += 1
+                case 2:
+                    p2_wipe_counter += 1
+                case 3:
+                    p3_wipe_counter += 1
+                case 4:
+                    p4_wipe_counter += 1
+                case 5:
+                    p5_wipe_counter += 1
+                case 6:
+                    p6_wipe_counter += 1
+
             graphql_query = f"""
             {{
                 reportData{{
@@ -125,31 +164,39 @@ for i in range(3,sheet.max_row):
             except:
                 deaths = query_fflogs(graphql_query)
             
-            death_counter = search_through_deaths_for_name(deaths, sheet[f"{COL_CES}2"].value)
+            death_counter = search_through_deaths_for_name(deaths, "Cesyanis Corvus")
             sheet[f"{COL_CES}{i}"] = int(death_counter) + int(sheet[f"{COL_CES}{i}"].value)
 
-            death_counter = search_through_deaths_for_name(deaths, sheet[f"{COL_JAMIE}2"].value)
+            death_counter = search_through_deaths_for_name(deaths, "Jamie Rosefall")
             sheet[f"{COL_JAMIE}{i}"] = int(death_counter) + int(sheet[f"{COL_JAMIE}{i}"].value)
 
-            death_counter = search_through_deaths_for_name(deaths, sheet[f"{COL_FAYTH}2"].value)
+            death_counter = search_through_deaths_for_name(deaths, "Fayth Delarosa")
             sheet[f"{COL_FAYTH}{i}"] = int(death_counter) + int(sheet[f"{COL_FAYTH}{i}"].value)
 
-            death_counter = search_through_deaths_for_name(deaths, sheet[f"{COL_EVE}2"].value)
+            death_counter = search_through_deaths_for_name(deaths, "Evlesk Soimer")
             sheet[f"{COL_EVE}{i}"] = int(death_counter) + int(sheet[f"{COL_EVE}{i}"].value)
 
-            death_counter = search_through_deaths_for_name(deaths, sheet[f"{COL_KAEDE}2"].value)
+            death_counter = search_through_deaths_for_name(deaths, "Kaede Yasashi")
             sheet[f"{COL_KAEDE}{i}"] = int(death_counter) + int(sheet[f"{COL_KAEDE}{i}"].value)
 
-            death_counter = search_through_deaths_for_name(deaths, sheet[f"{COL_MITHRIL}2"].value)
+            death_counter = search_through_deaths_for_name(deaths, "Mithril Dieties")
             sheet[f"{COL_MITHRIL}{i}"] = int(death_counter) + int(sheet[f"{COL_MITHRIL}{i}"].value)
 
-            death_counter = search_through_deaths_for_name(deaths, sheet[f"{COL_FERESIA}2"].value)
+            death_counter = search_through_deaths_for_name(deaths, "Feresia Peith")
             sheet[f"{COL_FERESIA}{i}"] = int(death_counter) + int(sheet[f"{COL_FERESIA}{i}"].value)
 
-            death_counter = search_through_deaths_for_name(deaths, sheet[f"{COL_RALF}2"].value)
+            death_counter = search_through_deaths_for_name(deaths, "Ra'nulf Reinhardt")
             sheet[f"{COL_RALF}{i}"] = int(death_counter) + int(sheet[f"{COL_RALF}{i}"].value)
 
         sheet[f"{COL_WIPES}{i}"] = int(fight_counter)
         sheet[f"{COL_AVERAGE_FIGHT_PERCENTAGE}{i}"] = float(sum/fight_counter)
+        sheet[f"{COL_BEST_FIGHT_PERCENTAGE}{i}"] = float(best_percentage)
+        sheet[f"{COL_PHASE1}{i}"] = int(p1_wipe_counter)
+        sheet[f"{COL_PHASE2}{i}"] = int(p2_wipe_counter)
+        sheet[f"{COL_PHASE3}{i}"] = int(p3_wipe_counter)
+        sheet[f"{COL_PHASE4}{i}"] = int(p4_wipe_counter)
+        sheet[f"{COL_PHASE_INT}{i}"] = int(int_wipe_counter)
+        sheet[f"{COL_PHASE5}{i}"] = int(p5_wipe_counter)
+        sheet[f"{COL_PHASE6}{i}"] = int(p6_wipe_counter)
 
 workbook.save(logs_excel)
